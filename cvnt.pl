67970a8740d007b0534840721a21c8214028e56f
daleif <daleif@3f4560be-fcb6-4516-b323-ae570ede3bb7>
2007-12-05 21:23:01 +0000

    license changed
    git-svn-id: http://subversion.assembla.com/svn/mhctan@15 3f4560be-fcb6-4516-b323-ae570ede3bb7

--- cvnt.pl
+++ cvnt.pl
@@ -1,12 +1,29 @@
 #!/usr/bin/perl
 
-# Copyright (C) 2007, Lars Madsen, daleif@imf.au.dk
-# 
-# usage: cvnt.pl file.lls > file.dtx
+# lls2dtx.pl
+# Copyright 2007 Lars Madsen
+#
+# This work may be distributed and/or modified under the
+# conditions of the LaTeX Project Public License, either version 1.3
+# of this license or (at your option) any later version.
+# The latest version of this license is in
+#   http://www.latex-project.org/lppl.txt
+# and version 1.3 or later is part of all distributions of LaTeX
+# version 2005/12/01 or later.
+#
+# This work has the LPPL maintenance status `maintained'.
+#
+# The Current Maintainer of this work is Lars Madsen, daleif@imf.au.dk
+#
+# This work consists of the Perl script lls2dtx.pl
+#
+# usage: lls2dtx.pl file.lls > file.dtx
 #
 # it will sort of convert the contents of file.lls into the syntax
 # used for dtx files, thought the output will need some hand-editing.
-
+#
+# HISTORY
+# 20071205 Initial script written, name schanged, and license added.
 
 use warnings;
 use strict;

feee8eac74089794531d11566c17af4d9275e31f
Will Robertson <wspr81@gmail.com>
2014-06-02 10:59:04 +0930

    unfortunately it seems these old sources won't compile any more :(

---
+++ historical/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+pdflatex "\pdfpagewidth=11in\relax\pdfpageheight=8.5in\relax\input{cmbase.lls}"
+
+## UNFORTUNATELY THE FOLLOWING SOURCES DON'T (OR NO LONGER?) COMPILE:
+
+#pdflatex "\pdfpagewidth=11in\relax\pdfpageheight=8.5in\relax\input{eqbreak.lls}"
+#pdflatex "\pdfpagewidth=11in\relax\pdfpageheight=8.5in\relax\input{flexisym.lls}"
+#pdflatex "\pdfpagewidth=11in\relax\pdfpageheight=8.5in\relax\input{breqn.lls}"
+#pdflatex "\pdfpagewidth=11in\relax\pdfpageheight=8.5in\relax\input{msabm.lls}"

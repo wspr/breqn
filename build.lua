bebc7da527cf02c462a7480db2ce9a0cb8c970b0
Will Robertson <wspr81@gmail.com>
2014-06-10 19:12:29 +0930

    trying to get the build script working for ctan installation :(

--- build.lua
+++ build.lua
@@ -6,6 +6,9 @@ module = "breqn"
 
 unpackfiles = {"*.dtx"}
 unpackopts  = "-interaction=batchmode"
+typesetopts = "-interaction=batchmode"
+binaryfiles = {"*.pdf","*.zip"}
+excludefiles = {"*/breqn-thesis.pdf","*/breqn-abbr-test.pdf","*/eqbreaks.pdf"}
 
 kpse.set_program_name("kpsewhich")
 buildscript = kpse.lookup("l3build.lua","mustexist")

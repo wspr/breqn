4bc2054d4b1950c0e1c32faf72280b4583587543
Will Robertson <wspr81@gmail.com>
2014-06-14 00:32:34 +0930

    touch up the build file

--- build.lua
+++ build.lua
@@ -5,11 +5,14 @@
 module = "breqn"
 
 unpackfiles = {"*.dtx"}
+
 unpackopts  = "-interaction=batchmode"
 typesetopts = "-interaction=batchmode"
 binaryfiles = {"*.pdf","*.zip"}
-excludefiles = {"*/breqn-thesis.pdf","*/breqn-abbr-test.pdf","*/eqbreaks.pdf"}
+excludefiles = {"*/breqn-thesis.pdf",
+                "*/breqn-abbr-test.pdf",
+                "*/eqbreaks.pdf"}
 
 kpse.set_program_name("kpsewhich")
-buildscript = kpse.lookup("l3build.lua","mustexist")
+buildscript = kpse.lookup("l3build.lua")
 dofile(buildscript)

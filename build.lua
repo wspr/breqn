df4d6ef5e453aa0ddf51f1e573f2587b613fd01b
Will Robertson <will@wspr.io>
2018-09-15 22:41:18 +0930

    update test files for TL2018

--- build.lua
+++ build.lua
@@ -1,7 +1,5 @@
 #!/usr/bin/env texlua
 
--- Build script for breqn
-
 module = "breqn"
 
 unpackfiles = {"*.dtx"}
@@ -9,13 +7,11 @@ installfiles = {"*.sty","*.sym"}
 
 unpackopts  = "-interaction=batchmode"
 typesetopts = "-interaction=batchmode"
+
 binaryfiles = {"*.pdf","*.zip"}
 excludefiles = {"*/breqn-thesis.pdf",
                 "*/breqn-abbr-test.pdf",
                 "*/eqbreaks.pdf"}
 
-packtdszip = true
-
-kpse.set_program_name("kpsewhich")
-buildscript = kpse.lookup("l3build.lua")
-dofile(buildscript)
+packtdszip   = true
+recordstatus = true

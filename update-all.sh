3019b37babf9fc821062516a8538c170c2f9bc6d
Frank Mittelbach <frank.mittelbach@latex-project.org>
2021-10-19 18:38:33 +0200

    update tests to current version of LaTeX (some of them are wrong obviously)

---
+++ update-all.sh
@@ -0,0 +1,31 @@
+
+l3build save -epdftex,xetex,luatex \
+  00-test-1 \
+  01-clash-1 \
+  01-clash-2 \
+  compact \
+  compact_w_slash \
+  condition \
+  dgroup_dmath_star \
+  dgroup_width \
+  frame \
+  framesep \
+  genfrac-1 \
+  hiderel \
+  indentstep \
+  lineno \
+  mathstyle-activechars \
+  mathstyle-default \
+  mathstyle-noactivechars \
+  number \
+  spread \
+  style
+
+
+
+l3build save -epdftex,luatex \
+  binom-frac \
+  genfrac-2
+
+l3build save  \
+  subsupstyle
